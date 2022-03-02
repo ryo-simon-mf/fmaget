d0952425985afa4fa7b9b517c994444804809fd6
Ryo_Simon <46313038+ryo-simon-mf@users.noreply.github.com>
2022-03-02 18:24:07 +0900

    Add files via upload

---
+++ fmaget4.py
@@ -0,0 +1,38 @@
+import os
+import time
+import datetime
+import re
+import requests
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+for page in range(20):
+    page += 1
+#     base = page + 12
+    url = "https://freemusicarchive.org/genre/Folk?sort=track_date_published&d=1&page=" + str(page)
+    headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; NP06; rv:11.0) like Gecko",
+            }
+
+    request = urllib.request.Request(url=url, headers=headers)
+    response = urllib.request.urlopen(request)
+    soup = BeautifulSoup(response,"html.parser")
+    response.close()
+    
+    elems = soup.find_all(href=re.compile("https://files.freemusicarchive.org/storage-freemusicarchive-org/music/"))
+    elenum =len(elems)
+    for onpage in range(elenum):
+        dl_url = elems[onpage].attrs['href']
+        print(dl_url)
+        # urllib.request.urlopen(dl_url)
+    
+        r_image = requests.get(dl_url)
+        filename_image = os.path.basename(dl_url)
+        with open('/home/ryo/Documents/fma/song/folk/' + filename_image, 'wb') as f:
+            f.write(r_image.content)
+
+        time.sleep(5)
+        dt = datetime.datetime.now()
+        print(dt)
+    time.sleep(10)
